7e593d886e1f51af301bd940cff540bef0e189ca
hieuttblackpink <trantronghieu369963@gmail.com>
2020-11-17 15:50:29 +0700

    add JUnit library

---
+++ test/hieu/util/test/MathUtilityTest.java
@@ -0,0 +1,43 @@
+
+package hieu.util.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MathUtilityTest 
+{
+    
+    public MathUtilityTest() 
+    {
+        
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() 
+    {
+        
+    }
+    
+    @Before
+    public void setUp() 
+    {
+        
+    }
+    
+    @After
+    public void tearDown()
+    {
+        
+    }
+
+}
