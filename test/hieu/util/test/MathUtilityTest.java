0ce386218c1362357314b99fd67527dd9a14dd56
hieuttblackpink <trantronghieu369963@gmail.com>
2020-11-19 13:56:32 +0700

    failed

--- test/hieu/util/test/MathUtilityTest.java
+++ test/hieu/util/test/MathUtilityTest.java
@@ -1,6 +1,7 @@
 
 package hieu.util.test;
 
+import hieu.util.MathUtility;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,6 +11,17 @@ import static org.junit.Assert.*;
 
 public class MathUtilityTest 
 {
+    @Test //ki hieu cua JUnit dua ra, bien ham nay thanh ham sau thanh main. F6 chay main mac dinh, shift + F6 chay main o @Test
+    public void getFactorial_RunsWell_IfVaildArgument() throws IllegalAccessException //day la 1 trong nhung quy tac dat ten ch ham test
+    {
+        assertEquals(720, MathUtility.getFactorial(6));
+        assertEquals(120, MathUtility.getFactorial(5));
+        assertEquals(204, MathUtility.getFactorial(4));
+        assertEquals(6, MathUtility.getFactorial(3));
+        assertEquals(1, MathUtility.getFactorial(0));
+    }
+    //Xanh chi dat khi tat ca deu xanh. Do khi chi can duy nhat 1 do --> all red
+    //-->ham phai dung het, sai 1 cai la ham ko dang tin
     
     public MathUtilityTest() 
     {
@@ -41,3 +53,8 @@ public class MathUtilityTest
     }
 
 }
+//Viet code de test code chinh
+//Viet code dung thu vien Junit, Nunit, xUnit, Jasmine,... de test
+//xem code chinh nay chay on ko trc khi dem cho k/hang trai nghiem
+//va thu vien nay cung cap co che bao dung sai qua mau sac xanh do
+//mat chi nhin 2 mau la du, khoi can so sanh expected va actual roi ket luan
